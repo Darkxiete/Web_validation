2c3e76da7fc8bfdda000837fd09481969b996978
Darkxiete <470058433@qq.com>
2020-06-01 15:14:51 +0800

    update clean_all_datas.py

--- clean_all_datas.py
+++ clean_all_datas.py
@@ -7,3 +7,6 @@ if __name__ == '__main__':
 	ex_cmd("rm -rf */logs/*")
 	ex_cmd("rm -rf */tmp/*")
 	ex_cmd("rm -rf */pics/*")
+	ex_cmd("rm -rf 4/noiseFigure/*")
+	ex_cmd("rm -rf 4/rawFigure/*")
+	ex_cmd("rm -rf 4/result_figure/*")
